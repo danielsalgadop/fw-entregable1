8f0fd7c73bb40c38e9c274aa1bd973e58be6d0a3
danielsalgadop <daniel.salgado.p@gmail.com>
2018-07-09 04:39:01 +0200

    return types

--- src/MyApp/Bundle/AppBundle/Controller/CalculatorController.php
+++ src/MyApp/Bundle/AppBundle/Controller/CalculatorController.php
@@ -25,7 +25,7 @@ class CalculatorController extends Controller
         $this->request = Request::createFromGlobals();
     }
 
-    public function addAction($param1, $param2)
+    public function addAction($param1, $param2): Response
     {
         if (! $this->validator->areNumbers(func_get_args())) {
             return $this->invalidParams();
@@ -33,7 +33,7 @@ class CalculatorController extends Controller
         return new Response((int)$this->calc->add($param1, $param2));
     }
 
-    public function substractAction()
+    public function substractAction(): Response
     {
         $param1 = $this->request->get('param1');
         $param2 = $this->request->get('param2');
@@ -43,7 +43,7 @@ class CalculatorController extends Controller
         return new Response((int)$this->calc->substract($param1, $param2));
     }
 
-    public function timesAction($param1)
+    public function timesAction($param1): Response
     {
         $param2 = $this->findParam2();
 
@@ -56,7 +56,7 @@ class CalculatorController extends Controller
     /**
      * @Route("/divide/{param1}/{param2}/", name="calculator_divide", requirements={"param1" = "-*\d+", "param2" = "-*\d+"})
      */
-    public function divideAction($param1, $param2)
+    public function divideAction($param1, $param2): Response
     {
         if ($param2 == 0) {
             return $this->render('default/error_406.html.twig', [
@@ -75,12 +75,12 @@ class CalculatorController extends Controller
         ]);
     }
 
-    public function errorAction()
+    public function errorAction(): Response
     {
         return $this->invalidParams();
     }
 
-    private function findParam2()
+    private function findParam2(): int
     {
         if ($this->request->request->get('param2')) {
             return $this->request->request->get('param2');
